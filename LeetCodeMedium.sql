7f12be80972fae04cebc9c59aa3a3e1ac4744063
Ferhat-Uzun <ferhat-uzun@outlook.com>
2022-11-24 00:58:12 +0300

    Added New Solutions

--- LeetCodeMedium.sql
+++ LeetCodeMedium.sql
@@ -13,3 +13,16 @@ group by u.user_id,u.join_date
 select score,
 dense_rank() OVER (order by score desc) as Rank
 from Scores
+
+--184. Department Highest Salary
+Select d.name as Department,e.name as Employee ,e.salary as Salary
+From Employee e
+Inner Join Department d on d.id = e.departmentId
+Where e.salary=  (Select Max(e.salary) 
+        From Employee e 
+        where e.departmentId= d.id)
+
+--180. Consecutive Numbers
+Select distinct l1.num as ConsecutiveNums
+From Logs l1,Logs l2,Logs l3 
+Where l1.id = l2.id-1 and l2.id=l3.id-1 and l1.num=l2.num and l2.num=l3.num
